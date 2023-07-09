fefe95dbc9647e7191ea6f26874000d22f3f242c
Niki wei <frank29580649@gmail.com>
2023-07-10 04:47:31 +0800

    波浪元件

---
+++ vue-project/src/components/Wave.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+<style lang="scss" scoped>
+.wave {
+  width: 100%;
+  height: 110px;
+  background-image: url("../assets/wave.png");
+  background-size: cover;
+  animation: slide-animation 5s linear infinite;
+}
+
+@keyframes slide-animation {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: -130px 0;
+  }
+}
+
+@media screen and (min-width: 1564px) {
+  div {
+    background-size: contain;
+  }
+}
+</style>
+<template>
+  <div class="wave"></div>
+</template>
